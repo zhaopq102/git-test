e8b5b240a63873694a3a34a67bc86da08b83ec53
Zhaopq <zhaopq@163.com>
2024-05-30 15:17:02 +0800

    Second master commit

--- src/main/java/com/zhaopq/GitTest.java
+++ src/main/java/com/zhaopq/GitTest.java
@@ -3,5 +3,6 @@ package com.zhaopq;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
