4bd8a2d6476a695282b2a7663db161c13d682e5a
Zhaopq <zhaopq@163.com>
2024-05-31 15:50:20 +0800

    login test

--- src/main/java/com/zhaopq/GitTest.java
+++ src/main/java/com/zhaopq/GitTest.java
@@ -1,4 +1,5 @@
 package com.zhaopq;
+import java.util.Scanner;
 
 public class GitTest {
     public static void main(String[] args) {
@@ -8,5 +9,22 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hot  fix test");
         System.out.println("master test");
+        String correctUsername = "user123";
+        String correctPassword = "password123";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("请输入用户名:");
+        String inputUsername = scanner.nextLine();
+
+        System.out.println("请输入密码:");
+        String inputPassword = scanner.nextLine();
+
+        if (correctUsername.equals(inputUsername) && correctPassword.equals(inputPassword)) {
+            System.out.println("登录成功!");
+        } else {
+            System.out.println("登录失败，用户名或密码错误!");
+        }
+        scanner.close();
     }
 }
